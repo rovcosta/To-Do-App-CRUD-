5e52d0867fc7be7d9cbdb8da1618797c9aff4efb
Roverson Costa <roversoncosta@MacBook-Air-de-Roverson.local>
2022-02-06 10:21:50 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+
+@app.route("/", methods=['GET', 'POST'] )
+def hello_world():
+    # 1 - Salva as informacoes preenchidas no banco de dados
+    if request.method=='POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+    # 2 - Busca as informacoes no banco de dados para mostrar na pagina
+    allTodo = Todo.query.all()
+    return render_template("index.html", allTodo=allTodo)
+# TO CREATE THE FILE .DB DO:
+# type python on terminal
+# >>> from app import db
+# >>> db.create_all()
+### A file named "tododb"
+
+
+@app.route("/update/<int:sno>", methods=['GET', 'POST'])
+def update(sno):
+    if request.method=='POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template('update.html', todo=todo)
+
+@app.route("/delete/<int:sno>")
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
