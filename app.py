82ac742956a8ef8f971b807394548a5721523bba
roverson <roversonfcosta@gmail.com>
2022-02-06 20:47:25 -0400

    try to fix error

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
@@ -17,10 +18,10 @@ class Todo(db.Model):
         return f"{self.sno} - {self.title}"
 
 
-@app.route("/", methods=['GET', 'POST'] )
+@app.route("/", methods=['GET', 'POST'])
 def hello_world():
     # 1 - Salva as informacoes preenchidas no banco de dados
-    if request.method=='POST':
+    if request.method == 'POST':
         title = request.form['title']
         desc = request.form['desc']
         todo = Todo(title=title, desc=desc)
@@ -29,6 +30,8 @@ def hello_world():
     # 2 - Busca as informacoes no banco de dados para mostrar na pagina
     allTodo = Todo.query.all()
     return render_template("index.html", allTodo=allTodo)
+
+
 # TO CREATE THE FILE .DB DO:
 # type python on terminal
 # >>> from app import db
@@ -38,7 +41,7 @@ def hello_world():
 
 @app.route("/update/<int:sno>", methods=['GET', 'POST'])
 def update(sno):
-    if request.method=='POST':
+    if request.method == 'POST':
         title = request.form['title']
         desc = request.form['desc']
         todo = Todo.query.filter_by(sno=sno).first()
@@ -50,7 +53,8 @@ def update(sno):
     todo = Todo.query.filter_by(sno=sno).first()
     return render_template('update.html', todo=todo)
 
-@app.route("/delete/<int:sno>")
+
+@app.route("/delete/<int:sno>", methods=['GET', 'POST'])
 def delete(sno):
     todo = Todo.query.filter_by(sno=sno).first()
     db.session.delete(todo)
@@ -58,5 +62,5 @@ def delete(sno):
     return redirect("/")
 
 
-if __name__ =="__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
